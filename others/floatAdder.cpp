5177b194bcb8b35153eaf7d39e88ec2e7708aa1f
Uirochan <haumuchan@gmail.com>
2023-11-22 23:02:11 +0900

    fixed issue, now work correctly

--- others/floatAdder.cpp
+++ others/floatAdder.cpp
@@ -18,7 +18,7 @@ uint32_t ALU_Adder(uint32_t a, uint32_t b){
 int main(){
 
     float f1 {1.5f} ;
-    float f2  {1.8f} ;
+    float f2  {10.5f} ;
 
     //uint32_tは符号なし３２ビットの整数
     std::optional<uint32_t> bit1 {ConvertFloatBitsToUint(f1)};
@@ -36,12 +36,13 @@ int main(){
     float answer {f1 + f2};
     uint32_t result   {ALU_Adder(a, b)};
 
-    std::cout << sizeof(result) << std::endl;;
+    std::cout << std::bitset<32>(result) << std::endl;
     
-    std::optional<float> f3 {ConvertBitsToFloat(result)};
+    std::optional<float> f3 {result};
+    std::optional<uint32_t> bit3 {ConvertFloatBitsToUint(f3.value())};
 
     if(f3){
         std::cout << "正しい答え: " << answer << std::endl;
-        std::cout << "計算結果: " << f3.value() << "(" << std::bitset<32>(f3.value()) << ")"<<std::endl;   
+        std::cout << "計算結果: " << f3.value() << "(" << std::bitset<32>(bit3.value()) << ")"<<std::endl;   
     }
 }
