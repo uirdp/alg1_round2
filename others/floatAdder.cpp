4ff1681bc38bd019600072d4a60189f26552becb
Uirochan <haumuchan@gmail.com>
2023-11-22 23:15:33 +0900

    some minor adjustments

--- others/floatAdder.cpp
+++ others/floatAdder.cpp
@@ -1,3 +1,10 @@
+/*論回でつくったALUの加算機にそのまま浮動小数点を突っ込んでみる。
+ *当然、仮数部から指数部（E -> Sも）への桁上げが発生すること、指
+ *数部の多きさによって仮数部の重みが違うことから、正しい演算結果
+ *は得られない 
+*/
+
+
 #include <iostream>
 #include <stdint.h>
 #include <optional>
@@ -18,7 +25,7 @@ uint32_t ALU_Adder(uint32_t a, uint32_t b){
 int main(){
 
     float f1 {2.5f} ;
-    float f2  {2.9f} ;
+    float f2  {10.9f} ;
 
     //uint32_tは符号なし３２ビットの整数
     std::optional<uint32_t> bit1 {ConvertFloatBitsToUint(f1)};
@@ -34,14 +41,14 @@ int main(){
     }
 
     float answer {f1 + f2};
-    uint32_t result   {ALU_Adder(a, b)};
 
-    std::cout << std::bitset<32>(result) << std::endl;
+    //this is intentionally wrong
+    uint32_t result   {ALU_Adder(a, b)};
     
     std::optional<float> f3 {ConvertBitsToFloat(result)};
     std::optional<uint32_t> bit3 {ConvertFloatBitsToUint(f3.value())};
 
-    if(f3){
+    if(f3 && bit3){
         std::cout << "正しい答え: " << answer << std::endl;
         std::cout << "計算結果: " << f3.value() << "(" << std::bitset<32>(bit3.value()) << ")"<<std::endl;   
     }
