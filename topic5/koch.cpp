1acaa673edb117f337124f47444217fb4384d769
Uirochan <haumuchan@gmail.com>
2023-12-11 21:39:22 +0900

    koch failed
    need to revise the entire algorithm just realised something

--- topic5/koch.cpp
+++ topic5/koch.cpp
@@ -1,17 +1,43 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 
+int n;
 using namespace std;
 
-float p = 100.0f;
-
 struct Point{
-    float x;
-    float y;
+    double x;
+    double y;
 };
 
-void koch(int n){
+void koch(Point p1, Point p2){
 
+    constexpr double pi =  3.141592653589793;
+    double d1 = p2.x - p1.x;
+
+    if(n == 0) return;
+
+    cout << p1.x << " " << p1.y << endl;
+    cout << p2.x << " " << p2.y << endl;
+    
+    Point s { d1/3, p1.x };
+    Point t { 2*d1/3, p1.x};
+
+    Point u{ d1/2, d1/3*sin(pi/3)};
+
+    cout << s.x << " " << s.y << endl;
+    cout << u.x << " " << u.y << endl;
+    cout << t.x << " " << t.y << endl;
+    
+    n--;
+    koch(s, u);
+    koch(u, t);
 }
 
+int main(){
+    Point p1 {0.0f, 0.0f};
+    Point p2 {100.0f, 0.0f};
 
+    cin >> n;
+    koch(p1, p2);
+}
 
